c6a9ed6dc81340edbf00420b5dc2f80b7cf48ae8
Mugema123 <mugemaleo@gmail.com>
2020-09-28 10:43:27 +0200

    changes

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require("express");
+
+
+const app = express();
+app.use(express.json());
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("connected to the server");
+});
+
+
